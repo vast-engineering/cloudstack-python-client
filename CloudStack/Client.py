ea5dc47eaac4622fdf160f370f27aee469330464
Michael Brock <mbrock@vast.com>
2016-09-14 11:34:35 -0500

    Remove duplicate "createAffinityGroup" function, remove "type" requirement from "deleteAffinityGroup" function.

--- CloudStack/Client.py
+++ CloudStack/Client.py
@@ -5249,24 +5249,6 @@ class Client(BaseClient):
 
     def createAffinityGroup(self, args={}):
         '''
-        Delete resource tag(s)
-
-        args - A dictionary. The following are options for keys:
-                name - name of the affinity group
-                type - Type of the affinity group from the available affinity/anti-affinity group types
-                account - an account for the affinity group. Must be used with domainId. Optional
-                description - optional description of the affinity group. Optional
-                domainid - domainId of the account owning the affinity group. Optional
-        '''
-        if not 'name' in args:
-            raise RuntimeError("Missing required argument 'name'")
-        if not 'type' in args:
-            raise RuntimeError("Missing required argument 'type'")
-
-        return self.request('createAffinityGroup', args)
-
-    def createAffinityGroup(self, args={}):
-        '''
         Create affinity group
 
         args - A dictionary. The following are options for keys:
@@ -5295,8 +5277,6 @@ class Client(BaseClient):
         '''
         if not 'name' in args:
             raise RuntimeError("Missing required argument 'name'")
-        if not 'type' in args:
-            raise RuntimeError("Missing required argument 'type'")
 
         return self.request('deleteAffinityGroup', args)
 
